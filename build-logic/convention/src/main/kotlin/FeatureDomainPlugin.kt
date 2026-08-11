3ca840b099de70358a2a1938d57dfbdb0b54b4bf
davit.takadze <davit.takadze@space.ge>
2026-08-11 16:14:12 +0400

    refactor FeatureDomainPlugin.kt

--- build-logic/convention/src/main/kotlin/FeatureDomainPlugin.kt
+++ build-logic/convention/src/main/kotlin/FeatureDomainPlugin.kt
@@ -1,4 +1,5 @@
 import com.space.chatapp.extensions.CORE_DOMAIN_MODULE
+import com.space.chatapp.extensions.implementationBundle
 import com.space.chatapp.extensions.implementationLibrary
 import com.space.chatapp.extensions.implementationModule
 import com.space.chatapp.extensions.testImplementationLibrary
@@ -14,9 +15,7 @@ class FeatureDomainPlugin : Plugin<Project> {
             dependencies {
                 implementationModule(CORE_DOMAIN_MODULE)
                 implementationLibrary("kotlinx-coroutines-core")
-                testImplementationLibrary("junit")
-                testImplementationLibrary("google-truth")
-                testImplementationLibrary("kotlinx-coroutines-test")
+                implementationBundle("test")
             }
         }
     }

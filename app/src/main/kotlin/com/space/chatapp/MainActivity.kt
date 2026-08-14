38ad5ab7d39a9f0ea27fa161648c174b54d5ba79
davit.takadze <davit.takadze@space.ge>
2026-08-15 02:12:28 +0400

    remove redundant imports

--- app/src/main/kotlin/com/space/chatapp/MainActivity.kt
+++ app/src/main/kotlin/com/space/chatapp/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.space.ui.theme.ChatAppTheme
 import com.space.ui.theme.ChatAppTheme.colors
-import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

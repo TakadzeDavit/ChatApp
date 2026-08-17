856ed6b1bb39083d2959c21c3d681b2ea05f7fb6
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-08-17 15:24:03 +0400

    changed base screen

--- core/presentation/src/main/kotlin/com/space/presentation/BaseScreen.kt
+++ core/presentation/src/main/kotlin/com/space/presentation/BaseScreen.kt
@@ -3,28 +3,22 @@ package com.space.presentation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import org.koin.compose.scope.KoinScope
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.core.annotation.KoinInternalApi
 import org.koin.core.parameter.ParametersDefinition
-import org.koin.core.qualifier.Qualifier
 
-@OptIn(KoinExperimentalAPI::class)
+@OptIn(KoinExperimentalAPI::class, KoinInternalApi::class)
 @Composable
 fun <UIState : UiState, UIEvent : UiEvent> BaseScreen(
     vmClass: VmClass<UIState, UIEvent>,
-    scopeQualifier: Qualifier,
     parameters: ParametersDefinition? = null,
     content: @Composable (state: UIState, onEvent: (UIEvent) -> Unit) -> Unit
 ) {
-    KoinScope(
-        scopeDefinition = { getOrCreateScope(scopeQualifier.value, scopeQualifier) }
-    ) {
-        val viewModel = koinViewModel(
-            vmClass = vmClass,
-            parameters = parameters
-        )
-        val state by viewModel.state.collectAsStateWithLifecycle()
-        NavCommands(viewModel.navigationCommands)
-        content(state, viewModel::onEvent)
-    }
+    val viewModel = koinViewModel(
+        vmClass = vmClass,
+        parameters = parameters
+    )
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    NavCommands(viewModel.navigationCommands)
+    content(state, viewModel::onEvent)
 }

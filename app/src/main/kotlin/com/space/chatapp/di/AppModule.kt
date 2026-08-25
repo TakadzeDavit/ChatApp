5f469e5f10a06a0d101c2d11b6e2f9f7681e91b3
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-08-25 12:29:38 +0400

    removed coreData module from appModule

--- app/src/main/kotlin/com/space/chatapp/di/AppModule.kt
+++ app/src/main/kotlin/com/space/chatapp/di/AppModule.kt
@@ -1,12 +1,10 @@
 package com.space.chatapp.di
 
-import com.space.core.data.di.coreDataModule
 import com.space.data.local.di.chatDataModule
 import org.koin.dsl.module
 
 val appModule = module {
     includes(
-        coreDataModule,
         chatDataModule
     )
 }
--- feature/chat/data/src/main/kotlin/com/space/data/local/di/ChatDataModule.kt
+++ feature/chat/data/src/main/kotlin/com/space/data/local/di/ChatDataModule.kt
@@ -1,5 +1,6 @@
 package com.space.data.local.di
 
+import com.space.core.data.di.coreDataModule
 import com.space.data.local.repository.ChatRepositoryImpl
 import com.space.domain.repository.ChatRepository
 import com.space.domain.scope.ChatScope
@@ -8,6 +9,7 @@ import org.koin.dsl.module
 import org.koin.plugin.module.dsl.scoped
 
 val chatDataModule = module {
+    includes(coreDataModule)
     scope<ChatScope.ParentScope> {
         scoped<ChatRepositoryImpl>() bind ChatRepository::class
     }

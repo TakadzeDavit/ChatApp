2d9fb3860845adf0907e82e4de8240b3067ec367
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-09-03 13:08:35 +0400

    removed unused code

--- feature/chat/data/src/main/kotlin/com/space/data/local/di/ChatDataModule.kt
+++ feature/chat/data/src/main/kotlin/com/space/data/local/di/ChatDataModule.kt
@@ -6,14 +6,9 @@ import com.space.data.local.mapper.MessageEntityMapperToResponse
 import com.space.data.local.mapper.MessageResponseMapperToEntity
 import com.space.data.local.repository.ChatRepositoryImpl
 import com.space.domain.repository.ChatRepository
-import com.space.domain.scope.ChatScope
-import com.space.domain.usecase.DeleteChatUseCase
-import com.space.domain.usecase.GetAllChatsUseCase
-import org.koin.dsl.*
+import org.koin.dsl.bind
 import org.koin.dsl.module
-import org.koin.plugin.module.dsl.scoped
 import org.koin.plugin.module.dsl.single
-import kotlin.math.sin
 
 val chatDataModule = module {
     includes(coreDataModule)
